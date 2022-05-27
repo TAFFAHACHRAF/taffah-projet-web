a977c89302677989a270de4c8a5834cc1dd8badc
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-05-27 22:00:03 +0200

    Add files via upload

---
+++ achraf taffah/prisma/migrations/20220527195109_taffah_three/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE `utilisateur` MODIFY `role` ENUM('USER', 'ADMIN', 'AUTHOR') NOT NULL DEFAULT 'AUTHOR';
+
+-- CreateTable
+CREATE TABLE `RefreshToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `expiredIn` INTEGER NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `RefreshToken_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `RefreshToken` ADD CONSTRAINT `RefreshToken_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Utilisateur`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
