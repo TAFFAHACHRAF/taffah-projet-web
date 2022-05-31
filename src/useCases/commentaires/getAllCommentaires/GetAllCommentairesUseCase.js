1e3c72e284eaac2b2a0ac00b3d6cbe8b29214340
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-05-31 17:54:12 +0200

    Delete src directory

--- src/useCases/commentaires/getAllCommentaires/GetAllCommentairesUseCase.js
+++
@@ -1,9 +0,0 @@
-const { client } = require("../../../prisma/client");
-
-class GetAllCommentairesUseCase {
-  async execute() {
-    const commentaires = await client.commentaire.findMany({});
-    return commentaires;
-  }
-}
-module.exports = { GetAllCommentairesUseCase };
